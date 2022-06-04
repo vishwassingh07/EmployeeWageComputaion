c8bae33c0c8bc478755947e079e1c2757da57c8f
Vishwas <baghel.vishwas0@gmail.com>
2022-06-04 17:05:08 +0530

    [Vishwas]add: Calculating Employee Wage For 20 Days Or 100 Days

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -10,7 +10,7 @@ namespace EmployeeWageComputation
     {
         const int IS_PRESENT = 0,WAGE_PER_HR = 20, FULL_DAY_HR = 8,PART_TIME_HR = 4,
         IS_FULL_DAY_PRESENT = 0, IS_PART_TIME_PRESENT = 1, WORKING_DAYS_A_MONTH = 20;
-        int empHrs, totalEmpWage, empWage;
+        int empHrs,totalEmpHrs, totalEmpWage, empWage, totalWorkingDays;
         public void EmployeeAttendence()
         {
             Random random = new Random();
@@ -26,7 +26,7 @@ namespace EmployeeWageComputation
         }
         public void EmpWageCalculation()
         {
-            for (int day = 0; day <= WORKING_DAYS_A_MONTH; day++)
+            for (int day = 0; day <= WORKING_DAYS_A_MONTH && empHrs<=100; day++)
             {
 
                 Random random = new Random();
@@ -44,10 +44,10 @@ namespace EmployeeWageComputation
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * WAGE_PER_HR;
-                totalEmpWage += empWage;
-                Console.WriteLine(empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day : {0}  Emp Hours : {1}", day, empHrs);
             }
+            int totalEmpWage = totalEmpHrs * WAGE_PER_HR;
             Console.WriteLine("Total Wage Of Employee is {0}", totalEmpWage);
 
 
