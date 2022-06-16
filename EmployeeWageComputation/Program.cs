184a1c3daa9832549c49a0e94a03c5c91f269b2d
Vishwas <baghel.vishwas0@gmail.com>
2022-06-16 10:44:37 +0530

    [Vishwas]add: Total Employee Wage For Each Company Program Added

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -10,7 +10,7 @@ namespace EmployeeWageComputation
             //attendancecheck.EmployeeAttendence();
             WageComputation empwagecomputation = new WageComputation();
             empwagecomputation.AddCompany("Reliance", 20, 8, 4, 100, 20);
-            empwagecomputation.AddCompany("Tata", 20, 8, 4, 100, 20);
+            empwagecomputation.AddCompany("Tata", 25, 9, 5, 90, 24);
             empwagecomputation.EmpWageCalculation("reliance");
             empwagecomputation.EmpWageCalculation("tata");
 
